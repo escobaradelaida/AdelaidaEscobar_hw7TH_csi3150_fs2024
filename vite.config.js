9371c89d33b1ea19759a5b7876d3a37ac1c95c6c
adelaida <adelaidaescobar@oakland.edu>
2024-12-02 20:53:04 -0500

    changes for it to work on GP

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  //add in for github pages
+  base: "/AdelaidaEscobar_hw7TH_csi3150_fs2024/",
+});
